0c0061f70321ecb726b5628939b5e2d2f90074f6
aniharriott <aninah@uw.edu>
2019-04-30 23:09:25 -0700

    created initial Graph files

---
+++ src/main/java/graph/GraphEdge.java
@@ -0,0 +1,45 @@
+package graph;
+
+import java.util.List;
+
+public class GraphEdge {
+
+    private String label;
+    private GraphNode parent;
+    private List<GraphNode> children;
+
+    // write a checkRep??
+
+    public GraphEdge(String l, GraphNode p, GraphNode c) {
+
+    }
+
+    public GraphEdge(String l, GraphNode p, List<GraphNode> c) {
+
+    }
+
+    public String getLabel() {
+        return null;
+    }
+
+    public GraphNode getParent() {
+        return null;
+    }
+
+    public List<GraphNode> getChildren() {
+        return null;
+    }
+
+    public void addChild(GraphNode c) {
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        return false;
+    }
+
+}
---
+++ src/main/java/graph/GraphNode.java
@@ -0,0 +1,37 @@
+package graph;
+
+import java.util.List;
+
+public class GraphNode {
+
+    private String label;
+    List<GraphEdge> edges;
+
+    public GraphNode(String l) {
+
+    }
+
+    public GraphNode(String l, List<GraphEdge> e) {
+
+    }
+
+    public GraphNode(String l, GraphEdge e) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public List<GraphEdge> getEdges() {
+        return null;
+    }
+
+    public void addEdge() {
+
+    }
+    
+    public String toString() {
+        return null;
+    }
+}
