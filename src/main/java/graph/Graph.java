eaccc57f1ba8fb638df4b03ede0a985a3cf6b3a2
aniharriott <aninah@uw.edu>
2019-05-02 11:54:32 -0700

    made edits to Graph

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,18 +1,19 @@
 package graph;
 
 import java.util.Set;
+import java.util.List;
 import org.apache.commons.lang3.NotImplementedException;
 
 /**
  * <b>Graph</b> represents a mutable directed graph represented by a
- * a set of <b>GraphNodes</b>.
+ * a set of unique <b>GraphNodes</b>.
  *
  * <p>A Graph can be empty but it cannot be null.
  */
 
 public final class Graph {
 
-    /** All the nodes in the graph in alphabetical order*/
+    /** All the nodes in the graph in alphabetical order */
     private final Set<GraphNode> nodes;
 
     // Abstraction Function:
@@ -40,36 +41,37 @@ public final class Graph {
         throw new NotImplementedException("Graph constructor not yet implemented");
     }
 
-    // SPEC EFFECTS? DUPLICATE EDGE?
     /**
      * Adds a new GraphEdge to this Graph
      *
      * @param n1 the source node of the GraphEdge to be added
      * @param n2 the destination node of the GraphEdge to be added
      * @param l the label of the GraphEdge to be added
-     * @spec.effects Creates a new GraphEdge from the given parameters
-     * @throws IllegalArgumentException if either GraphNode passed is not
-     * already contained within this Graph
+     * @spec.modifies n1 and n2
+     * @spec.effects Creates a new GraphEdge from the given parameters and associates it
+     * @throws IllegalArgumentException if either GraphNode passed is not already contained
+     * within this Graph, or if the edge already exists in this graph
      */
     public void addEdge(GraphNode n1, GraphNode n2, String l) {
         throw new NotImplementedException("addEdge not yet implemented");
     }
 
-    // WHAT IF A DUPLICATE NODE?
     /**
      * Adds a new GraphNode to this Graph
      *
      * @param n the GraphNode to be added
-     * @spec.requires n != null
+     * @spec.requires n != null, n.label cannot equal any other label in this graph
      * @spec.modifies this
      * @spec.effects If this.nodes = [n0, n1], then this_post.nodes = [n0, n1, n]
+     * @throws IllegalArgumentException if the label of n equals the label of a node
+     * already contained in this graph
      */
     public void addNode(GraphNode n) {
         throw new NotImplementedException("addNode not yet implemented");
     }
 
     /**
-     * Returns a set of all the GraphNodes in this Graph
+     * Returns a set of all the GraphNodes in this Graph in alphabetical order
      *
      * @return a Set</GraphNode> equal to this.nodes
      */
@@ -78,7 +80,7 @@ public final class Graph {
     }
 
     /**
-     * Returns a set of ??
+     * Returns a set of                                                                     //FIX THIS, ASK KATY
      *
      * @param n1
      * @param n2
@@ -86,6 +88,15 @@ public final class Graph {
      */
     public Set<GraphEdge> findPath(GraphNode n1, GraphNode n2) {
         throw new NotImplementedException("findPath not yet implemented");
+    }
+
+    /**
+     * Returns a list of all the GraphEdges in this Graph in alphabetical order
+     *
+     * @return a List</GraphNode> equal to all the edges contained in this graph
+     */
+    private List<GraphEdge> listEdges() {
+        throw new NotImplementedException("listEdges not yet implemented");
     }
 
 }
