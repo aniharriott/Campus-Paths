5babe3de19d5223f5982c904336ef057dba744dc
aniharriott <aninah@uw.edu>
2019-05-02 01:40:54 -0700

    made edits to GraphNode

--- src/main/java/graph/GraphNode.java
+++ src/main/java/graph/GraphNode.java
@@ -1,36 +1,59 @@
 package graph;
 
-import java.util.List;
+import java.util.Set;
 
-public class GraphNode {
+/**
+ * <b>GraphNode</b> is an mutable representation of a node on a Graph.
+ *
+ * <p>GraphNodes are represented a label and their child <b>GraphEdges</b>. A node is identifiable by its label
+ * which must be unique within a single <b>Graph</b>.
+ *
+ * <p>A GraphNode can have any number of GraphEdges, including zero.
+ */
 
+public final class GraphNode {
+
+    /** The label for this node */
     private String label;
-    List<GraphEdge> edges;
+    /** The edges that this node is a parent of */ // or a child of?? or not at all??
+    Set<GraphEdge> inComing;
+    Set<GraphEdge> outGoing;
 
+    /**
+     * @param l the label of the node to be constructed
+     * @spec.effects constructs a new node with a label l and no edges
+     */
     public GraphNode(String l) {
 
     }
 
-    public GraphNode(String l, List<GraphEdge> e) {
+    /** @spec.effects constructs a new node with a label l and edges e */
+    public GraphNode(String l, Set<GraphEdge> e) {
 
     }
 
+    /** @spec.effects constructs a new node with a label l and one edge e */
     public GraphNode(String l, GraphEdge e) {
 
+    }
+
+    public Set<GraphNode> getChildren() {
+        return null;
     }
 
     public String getName() {
         return null;
     }
 
-    public List<GraphEdge> getEdges() {
+    public Set<GraphEdge> getEdges() {
         return null;
     }
 
     public void addEdge() {
 
     }
-    
+
+    @Override
     public String toString() {
         return null;
     }
