bf92b23df95370308cf84ede2700bc1ba37a345e
aniharriott <aninah@uw.edu>
2019-05-14 13:42:50 -0700

    created Character java bean

---
+++ src/main/java/marvel/Character.java
@@ -0,0 +1,48 @@
+package marvel;
+
+import com.opencsv.bean.CsvBindByName;
+
+/**
+ * This is the Java representation of a Character
+ */
+public class Character {
+
+    /**
+     * name of the character
+     */
+    @CsvBindByName
+    private String name;
+
+    /**
+     * book the character appears in
+     */
+    @CsvBindByName
+    private String book;
+
+    /**
+     * get the name of the character
+     *
+     * @return name of the character
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set the name of the character
+     *
+     * @param name name of the character
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * get the book of the character
+     *
+     * @return book of the character
+     */
+    public String getBook() {
+        return this.book;
+    }
+}
